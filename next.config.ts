a592c8e26a899fb08d208dda1825a50261d12601
Erwin Wijaya <ichbinerwin99@gmail.com>
2025-10-24 08:15:13 +0700

    Add distDir and trailingSlash to next.config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   experimental: {
-    appDir: true, 
+    appDir: true,
   },
+  distDir: "dist",
+  trailingSlash: true, 
 };
 
 export default nextConfig;
