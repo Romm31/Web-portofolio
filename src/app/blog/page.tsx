20dfb74034ca0beb982a368df8001a5622fddbc8
Romm31 <ichbinerwin99@gmail.com>
2025-10-28 11:18:35 +0700

    Balikin /blog, kehapus cokk

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "@/components/blog/navbar";
+import { getAllBlogPosts, getAllTags } from "@/lib/mdx";
+import BlogClient from "./page-client"; 
+export default async function BlogPage() {
+  const allPosts = await getAllBlogPosts();
+  const allTags = await getAllTags();
+
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen bg-background pt-24">
+        <BlogClient allPosts={allPosts} allTags={allTags} />
+      </main>
+    </>
+  );
+}
